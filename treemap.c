19ad98d6c0b1dbf013948ba276d526e695712237
Acdiel1 <>
2021-05-07 18:41:16 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -80,10 +80,10 @@ void * searchTreeMap(TreeMap * tree, void* key) {
   TreeNode * aux= tree->root;
   if (key==aux->key){
     return aux->value;
-    aux=tree->current;
+    
   }
 
-
+aux=tree->current;
     if(key==NULL)
     return NULL;
 
