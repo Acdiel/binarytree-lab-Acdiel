a8a9afc1411220a14dab7e246c2e731225efb231
Acdiel1 <>
2021-05-07 20:51:20 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -50,16 +50,11 @@ TreeMap * createTreeMap(int (*lower_than) (void* key1, void* key2)) {
 
 
 void insertTreeMap(TreeMap * tree, void* key, void * value) {
+
   TreeNode * aux= tree->root;
 
- 
-    if (tree->lower_than(key, aux->key) == 1){
-       aux = aux->left;
-    }else{
-      if(tree->lower_than(key, aux->key) == 0){
-        aux = aux->right;
-      }
-    }
+    if (is_equal(tree,aux->key,key)== 1){
+      tree->current=aux;}
   
 
 }
