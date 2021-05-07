b403b96ea786736d2ffc5cd7ef128336062394c7
Acdiel1 <>
2021-05-07 20:09:58 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -88,7 +88,7 @@ void * searchTreeMap(TreeMap * tree, void* key) {
   tree->current->right=aux;
   tree->current->left=aux;
 
-  if(aux!=tree->current){
+  if(tree->current > aux){
     return NULL;
   }
 
